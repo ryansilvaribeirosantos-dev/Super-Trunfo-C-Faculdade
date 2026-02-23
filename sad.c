a33a92b28cedf3b6cf2fce258638e83f0ce34c66
ryansilvaribeirosantos-dev <ryansilvaribeirosantos@gmail.com>
2026-02-23 15:21:25 -0300

    Add files via upload

--- sad.c
+++ sad.c
@@ -1,45 +1,63 @@
 #include <stdio.h>
 
 int main() {
-    char estado;
-    char codigo[5];
-    char nomeCidade[50];
-    unsigned long int populacao;
-    int pontosTuristicos;
-    float area, pib, densidade, pibPerCapita;
+    // Carta 1
+    char estado1, codigo1[5], cidade1[50];
+    unsigned long int pop1;
+    float area1, pib1;
 
-    printf("--- Cadastro de Carta ---\n");
-    
-    printf("Estado (A-H): ");
-    scanf(" %c", &estado);
+    // Carta 2
+    char estado2, codigo2[5], cidade2[50];
+    unsigned long int pop2;
+    float area2, pib2;
 
-    printf("Codigo da Carta: ");
-    scanf("%s", codigo);
+    int opcao;
 
-    printf("Nome da Cidade: ");
-    scanf(" %[^\n]", nomeCidade);
+    // CADASTRO CARTA 1
+    printf("--- Cadastro Carta 1 ---\n");
+    printf("Estado (A-H): "); scanf(" %c", &estado1);
+    printf("Codigo: "); scanf("%s", codigo1);
+    printf("Cidade: "); scanf(" %[^\n]", cidade1);
+    printf("Populacao: "); scanf("%lu", &pop1);
+    printf("Area: "); scanf("%f", &area1);
 
-    printf("Populacao: ");
-    scanf("%lu", &populacao);
+    // CADASTRO CARTA 2
+    printf("\n--- Cadastro Carta 2 ---\n");
+    printf("Estado (A-H): "); scanf(" %c", &estado2);
+    printf("Codigo: "); scanf("%s", codigo2);
+    printf("Cidade: "); scanf(" %[^\n]", cidade2);
+    printf("Populacao: "); scanf("%lu", &pop2);
+    printf("Area: "); scanf("%f", &area2);
 
-    printf("Area (km2): ");
-    scanf("%f", &area);
+    // MENU DE COMPARAÇÃO
+    printf("\nEscolha o atributo para comparacao:\n");
+    printf("1. Populacao\n2. Area\nOpcao: ");
+    scanf("%d", &opcao);
 
-    printf("PIB (bilhoes): ");
-    scanf("%f", &pib);
-
-    printf("Pontos Turisticos: ");
-    scanf("%d", &pontosTuristicos);
-
-    // CALCULO CORRIGIDO
-    // Multiplicamos o PIB por 1 bilhao antes de dividir para evitar o 0.00
-    densidade = (float)populacao / area; 
-    pibPerCapita = (pib * 1000000000.0) / (float)populacao; 
-
-    printf("\n--- Resultados ---\n");
-    printf("Estado: %c | Codigo: %s | Cidade: %s\n", estado, codigo, nomeCidade);
-    printf("Densidade Populacional: %.2f hab/km2\n", densidade);
-    printf("PIB per Capita: %.2f reais\n", pibPerCapita);
+    switch(opcao) {
+        case 1:
+            printf("\nAtributo: Populacao\n");
+            if (pop1 > pop2) {
+                printf("Vencedor: %s (Carta 1)\n", cidade1);
+            } else if (pop2 > pop1) {
+                printf("Vencedor: %s (Carta 2)\n", cidade2);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+        case 2:
+            printf("\nAtributo: Area\n");
+            if (area1 > area2) {
+                printf("Vencedor: %s (Carta 1)\n", cidade1);
+            } else if (area2 > area1) {
+                printf("Vencedor: %s (Carta 2)\n", cidade2);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+        default:
+            printf("Opcao invalida.\n");
+    }
 
     return 0;
 }
